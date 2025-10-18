225e3474877c55a3da96d89b99fd00560594b694
YasyaYasinskaya <chudoyasya@gmail.com>
2025-10-18 19:44:56 +0300

    Add quess number

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$("#guess_number").click(function(){
+    let number=prompt("Яке число від 0 до 5 загадано?");
+    let numberToGuess=Math.floor(Math.random()*6);
+    if(number==numberToGuess){
+        alert("Правильно! Ти вгадав")
+    }
+    else{
+        alert(`Не вгадав! Було загадано число ${numberToGuess}`)
+    }
+})
